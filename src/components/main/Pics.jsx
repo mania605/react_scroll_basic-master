102305f008401d05e2400c832a527d077e2bf91c
mania605 <kim.minsun83@gmail.com>
2024-10-24 16:53:52 +0900

    스크롤 주석

--- src/components/main/Pics.jsx
+++ src/components/main/Pics.jsx
@@ -2,7 +2,7 @@ export default function Pics({ Scrolled }) {
 	console.log(Scrolled);
 	return (
 		<section className='pics'>
-			<div className='box' style={{ transform: `roate ${Scrolled / 2}deg)` }}></div>
+			<div className='box' style={{ transform: `rotate(${Scrolled / 10}deg)` }}></div>
 		</section>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,29 @@
+import { useState } from 'react';
 import Banner from '../components/main/Banner';
 import Pics from '../components/main/Pics';
 import Vids from '../components/main/Vids';
 import Visual from '../components/main/Visual';
+import { useEffect } from 'react';
 
 export default function Home() {
-
 	const [Scrolled, setScrolled] = useState(0);
-	console.log(Scrolled);
-	
-	//브라우저 스크롤 시 실행될 함수
-	// 내부적으로 현재 스크롤 되는 거리값을 실시간으로 Scrolled라는 state에 담아줌
+
+	// 브라우저 스크롤시 실행될 함수
+	// 내부적으로 현재 스크롤되는 거리값을 실시간으로 Scrolled라는 state에 담아줌
 	const handleScroll = () => {
 		setScrolled(window.scrollY);
 	};
 
-
-
-useEffect(()=>{
-	//컴포넌트 마운트시 윈도우 객체의 
-	window.addEventListener('scroll', handleScroll);
-	redirectDocument()=>window.removeEventListener('scroll', handleScroll);
-},[]);
+	useEffect(() => {
+		// 컴포넌트 마운트시 윈도우 객체의 scroll 이벤트에 handleScroll 함수 연결
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
 
 	return (
 		<main>
 			<Visual />
-			<Pics Scrolled={Scrolled}/>
+			<Pics Scrolled={Scrolled} />
 			<Vids />
 			<Banner />
 		</main>
