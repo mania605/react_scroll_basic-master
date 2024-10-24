f8da4827fdc4b6b7013f2483e8342b0ff83d6cba
mania605 <kim.minsun83@gmail.com>
2024-10-24 17:22:02 +0900

    브라우저 리사이즈시 해당 자식 섹션의 세로 위치값을 참조객체 배열에 담아주는 함수

--- src/components/main/Pics.jsx
+++ src/components/main/Pics.jsx
@@ -2,7 +2,7 @@ export default function Pics({ Scrolled }) {
 	console.log(Scrolled);
 	return (
 		<section className='pics'>
-			<div className='box' style={{ transform: `rotate(${Scrolled / 10}deg)` }}></div>
+			<div className='box' style={{ transform: `rotate(${Scrolled / 5}deg)` }}></div>
 		</section>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,34 @@
-import { useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import Banner from '../components/main/Banner';
 import Pics from '../components/main/Pics';
 import Vids from '../components/main/Vids';
 import Visual from '../components/main/Visual';
-import { useEffect } from 'react';
 
 export default function Home() {
 	const [Scrolled, setScrolled] = useState(0);
+	const ref_el = useRef(null);
+	const ref_posArr = useRef([]);
 
-	// 브라우저 스크롤시 실행될 함수
-	// 내부적으로 현재 스크롤되는 거리값을 실시간으로 Scrolled라는 state에 담아줌
+	//브라우저 리사이즈시 해당 메일 프레임의 자식 요소를 반복돌며 각요소의 세로위치값을 ref_posArr에 담아주는 함수
+	const getPos = () => {
+		ref_posArr.current = [];
+		console.log(ref_el.current.childern);
+		for (const el of ref_el.current.childern) ref_posArr.current.push(el.offsetTop);
+		console.log(ref_posArr.current.childern);
+	};
+
 	const handleScroll = () => {
+		// 브라우저 스크롤시 실행될 함수: 내부적으로 현재 스크롤되는 거리값을 실시간으로 Scrolled라는 state에 담아줌
 		setScrolled(window.scrollY);
 	};
 
+	//성능과 메모리 과다사용 방지를 위해 꼭 필요한 작업.
 	useEffect(() => {
 		// 컴포넌트 마운트시 윈도우 객체의 scroll 이벤트에 handleScroll 함수 연결
+		window.addEventListener('resize', getPos);
 		window.addEventListener('scroll', handleScroll);
-		return () => window.removeEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('resize', getPos);
+		window.removeEventListener('scroll', handleScroll);
 	}, []);
 
 	return (
