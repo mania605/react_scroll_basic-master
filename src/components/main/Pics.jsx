7bad93b86f6a841d6eeaf8d5d307bc936e8fa9a2
mania605 <kim.minsun83@gmail.com>
2024-10-24 17:47:25 +0900

    코드는 엉망 주석만 볼것

--- src/components/main/Pics.jsx
+++ src/components/main/Pics.jsx
@@ -1,8 +1,10 @@
-export default function Pics({ Scrolled }) {
-	console.log(Scrolled);
+export default function Pics({ Scrolled, pos }) {
 	return (
 		<section className='pics'>
-			<div className='box' style={{ transform: `rotate(${Scrolled / 5}deg)` }}></div>
+{/* pos ||0의 의미는 초기에 pos값이 undefined이면 일단은 0으로 대신 값을 집어넣음 */}
+{/* pos가 usRef로 참조된 값이므로 첫번째 렌더링시에 pos값이 undefined로 나오면서 - 연산에서 발생 */}
+			<h2 style {{left: Scrolled-pos || 0}}>TYPOGRAPHY</h2>
+			<div className='box' style={{ transform: `rotate(${Scrolled - pos}deg)` }}></div>
 		</section>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,25 +6,26 @@ import Visual from '../components/main/Visual';
 
 export default function Home() {
 	const [Scrolled, setScrolled] = useState(0);
+	const [PosArr, setPosArr] = useState([]);
 	const ref_el = useRef(null);
 	const ref_posArr = useRef([]);
 
 	//브라우저 리사이즈시 해당 메일 프레임의 자식 요소를 반복돌며 각요소의 세로위치값을 ref_posArr에 담아주는 함수
 	const getPos = () => {
 		ref_posArr.current = [];
-		console.log(ref_el.current.childern);
 		for (const el of ref_el.current.childern) ref_posArr.current.push(el.offsetTop);
-		console.log(ref_posArr.current.childern);
+		console.log(ref_posArr.current);
 	};
 
+	// 브라우저 스크롤시 실행될 함수: 내부적으로 현재 스크롤되는 거리값을 실시간으로 Scrolled라는 state에 담아줌
 	const handleScroll = () => {
-		// 브라우저 스크롤시 실행될 함수: 내부적으로 현재 스크롤되는 거리값을 실시간으로 Scrolled라는 state에 담아줌
 		setScrolled(window.scrollY);
 	};
 
 	//성능과 메모리 과다사용 방지를 위해 꼭 필요한 작업.
 	useEffect(() => {
 		// 컴포넌트 마운트시 윈도우 객체의 scroll 이벤트에 handleScroll 함수 연결
+		getPos();
 		window.addEventListener('resize', getPos);
 		window.addEventListener('scroll', handleScroll);
 		return () => {
@@ -36,7 +37,9 @@ export default function Home() {
 	return (
 		<main>
 			<Visual />
-			<Pics Scrolled={Scrolled} />
+			{/* 해당 Home컴포넌트에서 만들어지는 자식 섹션들의 세로 위치 배열값 중 2번째에 해당하는 Pics의 위치값만 pos라는 이름의 props로 전달*/}
+			{/* pos값이 필요한 이유 해당 컴포넌트는 문서 상단에 위치해 있지 않기 때문에 현재 스크롤되고 있는 거리값에서 섹션의 위치값을 빼줘야지 0 으로 초기화된 값으로 스타일 적용 가능 */}
+			<Pics Scrolled={Scrolled} pos={ref_posArr.current[1]} />
 			<Vids />
 			<Banner />
 		</main>
